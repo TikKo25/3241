7daa2b12b2200c657b743458f76ad61251e84ad5
TikKo25 <tixon250904@gmail.com>
2024-10-13 17:19:43 +0300

    Add files via upload

--- Задание 6.1.cpp
+++ Задание 6.1.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
-#include <algorithm>
-#include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
+int swaps = 0;
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+            swaps++;
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    swaps++;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
 int main(int argc, char* argv[]) {
-    const int MAX_SIZE = 62;
-    double numbers[MAX_SIZE];
-    int numCount = 0;
-
-    for (int i = 1; i < argc && numCount < MAX_SIZE; i++) {
-        double num = atof(argv[i]);
-        if (num == 0) break;
-        numbers[numCount++] = num;
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " <number1> <number2> ... <numberN>" << endl;
+        return 1;
     }
 
-    for (int i = argc; cin >> numbers[numCount++];);
-    numCount--;
+    int size = argc - 1;
+    int numbers[size];
 
-    if (numCount < MAX_SIZE) {
-        cout << "Number of entered values: " << numCount << endl;
+    for (int i = 1; i < argc; i++) {
+        numbers[i - 1] = atoi(argv[i]);
     }
 
-    sort(numbers, numbers + numCount);
-    cout << ": ";
-    for (int i = 0; i < numCount; i++) {
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
         cout << numbers[i] << " ";
     }
     cout << endl;
 
-    double sum = 0;
-    for (int i = 0; i < numCount; i++) {
-        sum += numbers[i];
-    }
-    double mean = sum / numCount;
-    cout << "Mean: " << mean << endl;
+    quickSort(numbers, 0, size - 1);
 
-    double median;
-    if (numCount % 2 == 0) {
-        median = (numbers[numCount / 2 - 1] + numbers[numCount / 2]) / 2;
+    cout << "Sorted array: ";
+    for (int i = 0; i < size; i++) {
+        cout << numbers[i] << " ";
     }
-    else {
-        median = numbers[numCount / 2];
-    }
-    cout << "Median: " << median << endl;
+    cout << endl;
 
-    double variance = 0;
-    for (int i = 0; i < numCount; i++) {
-        variance += (numbers[i] - mean) * (numbers[i] - mean);
-    }
-    variance /= numCount;
-    double stddev = sqrt(variance);
-    cout << "Standard deviation: " << stddev << endl;
-
-    cout << "Minimum: " << numbers[0] << endl;
-    cout << "Maximum: " << numbers[numCount - 1] << endl;
+    cout << "Number of swaps: " << swaps << endl;
+    cout << "Pivot element: " << numbers[swaps] << endl;
 
     return 0;
 }
